dee8f162b6940e28dbe2fb0ba8de5740ab7f149e
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-02-02 08:52:47 -0300

    novas dependencias

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,26 @@
 import axios from 'axios'
-import React from 'react';
 import { AsyncStorage } from 'react-native';
-var ip;
-async function teste() {
-     ip = await AsyncStorage.getItem('ip');
-    console.log("aaaaaaaa", ip);
-
+var test = ''
+async function teste ()  {
+  var base =  await AsyncStorage.getItem("ip", (ip) => {
+    console.log("ip:",base);
+    });
+    console.log("base: ",base);
+    test = base;
 }
+teste()
+var ip = '192.168.3.100';
+var api;
+if(api === undefined)
+{
+     api = axios.create({
 
-
-const api = axios.create({
-
-    baseURL: `http://192.168.3.100:3333`,
+    baseURL: `http://${ip}:3333`,
 });
 
+}
+const ret = api;
+
+export default  ret;
 
 
-
-export default api;
