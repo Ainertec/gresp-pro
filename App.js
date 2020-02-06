a6a57a50427168c3bf4a931e39533e6d9be26717
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-02-06 07:58:05 -0300

    remoção de alertas

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, YellowBox } from 'react-native'
 import Routes from './src/routes';
 import Dialog from 'react-native-dialog'
 
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+  'componentWillReceiveProps'
+])
 export default function App() {
   return <Routes/>
     
