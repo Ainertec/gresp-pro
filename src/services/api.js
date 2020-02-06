88702deb17129a64cce8495c990eeed2060ef41c
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-02-06 09:27:56 -0300

    adicionado ip dinamico

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,15 @@
 import axios from 'axios'
 import { AsyncStorage } from 'react-native';
-var test = ''
-async function teste ()  {
-  var base =  await AsyncStorage.getItem("ip", (ip) => {
-    console.log("ip:",base);
-    });
-    console.log("base: ",base);
-    test = base;
-}
-teste()
-var ip = '192.168.3.100';
-var api;
-if(api === undefined)
-{
-     api = axios.create({
 
-    baseURL: `http://${ip}:3333`,
-});
+export default async function api(){
+  let ip = await AsyncStorage.getItem('ip');
+
+  let api = axios.create({
+    baseURL:`http://${ip}:3333`,
+  });
+
+  return api;
 
 }
-const ret = api;
-
-export default  ret;
 
 
