48e59ad01a7bedc3db8fadfcc5e27a0e2efc94a7
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-06-08 19:20:43 -0300

    Delete

--- backend/src/model/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String,
-});
-
-module.exports = mongoose.model("Product", ProductSchema);
