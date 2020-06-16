5a580418269faa957e1c43a00f3f82401cf40d1e
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-06-16 09:41:06 -0300

    Feat: add setting page

--- mobile/src/pages/Setting/index.js
+++ mobile/src/pages/Setting/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import { useAuth } from '../../contexts/auth';
+
+import { Container, Content, IconArea, ContentText, Title } from './styles';
 
 const Setting = () => {
-  return <Container />;
+  const { signOut } = useAuth();
+  return (
+    <Container>
+      <Title>Configurações</Title>
+
+      <Content onPress={() => signOut()} style={{ elevation: 90 }}>
+        <IconArea>
+          <Icon name='log-out' size={25} color='#Fff' />
+        </IconArea>
+        <ContentText>Sair</ContentText>
+      </Content>
+
+      <Content onPress={() => {}} style={{ elevation: 90 }}>
+        <IconArea>
+          <Icon name='wifi' size={25} color='#Fff' />
+        </IconArea>
+        <ContentText>Rede</ContentText>
+      </Content>
+    </Container>
+  );
 };
 
 export default Setting;
--- mobile/src/pages/Setting/styles.js
+++ mobile/src/pages/Setting/styles.js
@@ -3,3 +3,26 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+export const Content = styled.TouchableOpacity`
+  flex-direction: row;
+  background: #3f173f;
+
+  align-items: center;
+  padding: 10px;
+  border-radius: 19px;
+  margin: 15px 8px;
+`;
+export const IconArea = styled.View`
+  padding: 0 12px;
+`;
+export const ContentText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+`;
+export const Title = styled.Text`
+  padding-top: 10px;
+  color: black;
+  align-self: center;
+  font-weight: bold;
+  font-size: 24px;
+`;
